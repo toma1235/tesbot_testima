55f5c6c8fe6cde70b4d72b6d60f4243bd9912ec5
toma1235 <tomasrocket17@gmail.com>
2024-11-20 21:57:31 +0100

    helo

--- bot.py
+++ bot.py
@@ -80,6 +80,12 @@ def key(message):                                          # Bottoni che quando 
   
   
   
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)  
+  
+  
   
   
   
